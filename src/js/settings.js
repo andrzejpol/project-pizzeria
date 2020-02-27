6d49a5dcd28ef9eafb31808590c84eb6411e5d8d
Andrzej Polak <andrzejp2805@gmail.com>
2020-02-27 18:06:10 +0100

    Add subpages

--- src/js/settings.js
+++ src/js/settings.js
@@ -1,15 +1,17 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable no-undef */
 
-
 export const select = {
   templateOf: {
     menuProduct: '#template-menu-product',
     cartProduct: '#template-cart-product',
+    bookingWidget: '#template-booking-widget',
   },
   containerOf: {
     menu: '#product-list',
     cart: '#cart',
+    pages: '#pages',
+    booking: '.booking-wrapper',
   },
   all: {
     menuProducts: '#product-list > .product',
@@ -30,7 +32,17 @@ export const select = {
       linkDecrease: 'a[href="#less"]',
       linkIncrease: 'a[href="#more"]',
     },
+    datePicker: {
+      wrapper: '.date-picker',
+      input: `input[name="date"]`,
+    },
+    hourPicker: {
+      wrapper: '.hour-picker',
+      input: 'input[type="range"]',
+      output: '.output',
+    },
   },
+
   cart: {
     productList: '.cart__order-summary',
     toggleTrigger: '.cart__summary',
@@ -45,10 +57,20 @@ export const select = {
   },
   cartProduct: {
     amountWidget: '.widget-amount',
+
     price: '.cart__product-price',
     edit: '[href="#edit"]',
     remove: '[href="#remove"]',
   },
+  booking: {
+    peopleAmount: '.people-amount',
+    hoursAmount: '.hours-amount',
+    tables: '.floor-plan .table',
+  },
+  nav: {
+    links: '.main-nav a',
+  },
+
 };
 
 export const classNames = {
@@ -56,28 +78,61 @@ export const classNames = {
     wrapperActive: 'active',
     imageVisible: 'active',
   },
+
   cart: {
     wrapperActive: 'active',
-  }
+  },
+
+  booking: {
+    loading: 'loading',
+    tableBooked: 'booked',
+  },
+  nav: {
+    active: 'active',
+  },
+  pages: {
+    active: 'active',
+  },
 };
 
 export const settings = {
+  hours: {
+    open: 12,
+    close: 24,
+  },
   amountWidget: {
     defaultValue: 1,
     defaultMin: 1,
     defaultMax: 9,
   },
+  datePicker: {
+    maxDaysInFuture: 14,
+  },
+
   cart: {
     defaultDeliveryFee: 20,
+  },
+
+  booking: {
+    tableIdAttribute: 'data-table',
   },
   db: {
     url: '//localhost:3131',
     product: 'product',
     order: 'order',
+    booking: 'booking',
+    event: 'event',
+    dateStartParamKey: 'date_gte',
+    dateEndParamKey: 'date_lte',
+    notRepeatParam: 'repeat=false',
+    repeatParam: 'repeat_ne=false',
   },
 };
 
 export const templates = {
   menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
+
   cartProduct: Handlebars.compile(document.querySelector(select.templateOf.cartProduct).innerHTML),
+
+  bookingWidget: Handlebars.compile(document.querySelector(select.templateOf.bookingWidget).innerHTML),
 };
